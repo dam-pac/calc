52144b7a97ee26fe4e8309028895e4386b5638ec
dam-pac <83549411+dam-pac@users.noreply.github.com>
2021-12-19 19:41:55 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,118 @@
+print('Installing...')
+print('Installing moduls')
+import os
+import platform
+a = platform.system()
+if platform.system() == 'Windows':
+    cls = 'cls'
+    rm = 'del'
+    pera = 'pip install -r requirements.txt'
+    pip = 'timeout 1'
+    pipr = 'pip install requests'
+    pstart = 'calc.py'
+else:
+    cls = 'clear'
+    rm = 'rm'
+    pip = 'sudo apt install python3-pip'
+    pera = 'python3 pip install -r requirements.txt'
+    pipr = 'python3 pip install requests'
+    pstart = 'python3 calc.py'
+os.system(pipr)
+try:
+    f = open('requirements.txt')
+    f.close()
+    print('requirements success!')
+except FileNotFoundError:
+    print('Downloading requirements.txt')
+    import requests
+    f = open(r'requirements.txt',"wb")
+    ufr = requests.get("http://calc.makkate.ru/requirements.txt")
+    f.write(ufr.content)
+    f.close()
+os.system(pip)
+os.system(pera)
+print('Checking...')
+while True:
+    try:
+        from colorama import Fore, Back, Style
+        import os
+        import time
+        import wave
+        import sys
+        import itertools
+        import threading
+        import math
+        import platform
+        from playsound import playsound
+        from asciimatics import *
+        from russian import *
+        from english import *
+        from datetime import date
+        from datetime import datetime
+        import requests
+        break
+    except ModuleNotFoundError:
+        print('ERROR import modules, trying installing...')
+        os.system(pera)
+        break
+print('Check files...')
+try:
+    f = open('calc.py')
+    f.close()
+    print('calc.py success!')
+except FileNotFoundError:
+    print('Downloading calc.py')
+    import requests
+    f = open(r'calc.py',"wb")
+    ufr = requests.get("http://calc.makkate.ru/calc.py")
+    f.write(ufr.content)
+    f.close()
+try:
+    f = open('english.py')
+    f.close()
+    print('english.py success!')
+except FileNotFoundError:
+    print('Downloading english.py')
+    import requests
+    f = open(r'english.py',"wb")
+    ufr = requests.get("http://calc.makkate.ru/english.py")
+    f.write(ufr.content)
+    f.close()
+try:
+    f = open('russian.py')
+    f.close()
+    print('russian.py success!')
+except FileNotFoundError:
+    print('Downloading russian.py')
+    import requests
+    f = open(r'russian.py',"wb")
+    ufr = requests.get("http://calc.makkate.ru/russian.py")
+    f.write(ufr.content)
+    f.close()
+try:
+    f = open('start.wav')
+    f.close()
+    print('start.wav success!')
+except FileNotFoundError:
+    print('Downloading start.wav')
+    import requests
+    f = open(r'start.wav',"wb")
+    ufr = requests.get("http://calc.makkate.ru/start.wav")
+    f.write(ufr.content)
+    f.close()
+try:
+    f = open('shutdown.mp3')
+    f.close()
+    print('shutdown.mp3 success!')
+except FileNotFoundError:
+    print('Downloading shutdown.mp3')
+    import requests
+    f = open(r'shutdown.mp3',"wb")
+    ufr = requests.get("http://calc.makkate.ru/shutdown.mp3")
+    f.write(ufr.content)
+    f.close()
+print('Install ended! Start calc.py')
+os.system(rm + ' install.py')
+os.system(cls)
+os.system(pstart)
+exit()
